4c4980d4f3402cdfe2371e909de4ece6588140d2
Vinícius <vinicius.matheus252@gmail.com>
2020-10-05 21:18:35 -0300

    Fix main activity window controls

--- AppEmpresas/app/src/main/java/io/github/vnicius/appempresas/ui/main/MainActivity.kt
+++ AppEmpresas/app/src/main/java/io/github/vnicius/appempresas/ui/main/MainActivity.kt
@@ -2,7 +2,9 @@ package io.github.vnicius.appempresas.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.commit
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.commitNow
+import io.github.vnicius.appempresas.R
 import io.github.vnicius.appempresas.databinding.ActivityMainBinding
 import io.github.vnicius.appempresas.extension.setTranslucentWindowControls
 import io.github.vnicius.appempresas.extension.setupFullScreenSystemUiFlags
@@ -10,21 +12,26 @@ import io.github.vnicius.appempresas.ui.search.SearchFragment
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var viewBiding: ActivityMainBinding
+    private lateinit var viewBinding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewBiding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(viewBiding.root)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
 
         setupFullScreenSystemUiFlags()
-        setTranslucentWindowControls(false, true)
+        setTranslucentWindowControls(
+            navigationBarColor = ContextCompat.getColor(
+                baseContext,
+                R.color.colorDefaultNavigationBar
+            ), withLightStatusBar = false, withLightNavigationBar = true
+        )
         navigateToSearchScreen()
     }
 
     private fun navigateToSearchScreen() {
-        supportFragmentManager.commit {
-            add(viewBiding.container.id, SearchFragment.newInstance())
+        supportFragmentManager.commitNow {
+            add(viewBinding.container.id, SearchFragment.newInstance())
         }
     }
 }
